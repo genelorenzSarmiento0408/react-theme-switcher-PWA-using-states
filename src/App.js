33bd1328b06d9d8ef4dd2c032c456f8542ae7347
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2022-02-02 17:16:30 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function App() {
+  const [isDark, setIsDark] = useState(false);
+  const styles = {
+    background: isDark ? "#2f2f2f" : "transparent",
+    color: isDark ? "white" : "black",
+  };
+  return (
+    <div style={styles} className="App">
+      <h1>Theme Switcher</h1>
+      <button onClick={() => setIsDark((prevTheme) => !prevTheme)}>
+        Turn to {isDark ? "light" : "dark"} mode
+      </button>
+    </div>
+  );
+}
+
+export default App;
